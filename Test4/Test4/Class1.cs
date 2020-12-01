198426d8899d915c2dff42869a898a412b4f503a
Shreepad Divekar <54166955+Shaden27@users.noreply.github.com>
2020-12-01 10:31:19 +0530

    Add files via upload
    I am still confused about the implementation of some functions. I have not implemented search and sort.

---
+++ Test4/Test4/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4
+{
+    class Class1
+    {
+        protected virtual void func()
+        {
+            Console.WriteLine("HEllo Func 1");
+        }
+    }
+    class Class2:Class1
+    {
+        protected override void func()
+        {
+            Console.WriteLine("HEllo Func 2");
+        }
+    }
+    abstract class abs
+    {
+        protected abstract void func();
+    } 
+}
